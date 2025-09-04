a992144f4ed5ca10ab4ff025ff1be3802365c6fc
gh369-639 <hocy36999@gmail.com>
2025-09-04 14:02:08 +0700

    up

--- Gemini-Baidu-translate-change-model/src/translate.js
+++ Gemini-Baidu-translate-change-model/src/translate.js
@@ -25,7 +25,7 @@ function generateFingerprintCacheKey(lines) {
 }
 
 function manageCacheAndSave(cacheKey, contentToSave) {
-    const MAX_CACHE_SIZE = 50;
+    const MAX_CACHE_SIZE = 35;
     const CACHE_MANIFEST_KEY = "vbook_cache_manifest";
 
     try {
