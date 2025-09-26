a44224d59ffae15534842922bb72ec91335ca2dd
639936 <hoahockibi@gmail.com>
2025-09-26 10:38:11 +0700

    transv6

--- cosplaytele/src/chap.js
+++ cosplaytele/src/chap.js
@@ -3,13 +3,16 @@ load('config.js');
 function execute(url) {
     url = url.replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img, BASE_URL);
 
-    var doc = Http.get(url).html();
-    var el = doc.select("#gallery-1 .gallery-item img");
-    var data = [];
-    for (var i = 0; i < el.size(); i++) {
-        var e = el.get(i);
-        var imgs = e.attr("src");
-        data.push(imgs);
+    let reschap =  fetch(url);
+    if (reschap.ok) {
+        var el = reschap.html().select("#gallery-1 .gallery-item img");
+        var data = [];
+        for (var i = 0; i < el.size(); i++) {
+            var e = el.get(i);
+            var imgs = e.attr("src");
+            data.push(imgs);
+        }
+        return Response.success(data);
     }
-    return Response.success(data);
+    return null;
 }
